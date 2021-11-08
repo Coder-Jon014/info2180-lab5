3331d071a14de682fa54b1c4fccc9998f5845cb5
Coder-Jon014 <jon.coombs14@gmail.com>
2021-11-08 01:27:23 -0500

    Completed files

---
+++ world.js
@@ -0,0 +1,45 @@
+window.onload=function(){
+    let countryLookupbtn = document.getElementById("lookup");
+    let cityLookupbtn = document.getElementById("lookup-city")
+    var httpsRequest;
+
+    countryLookupbtn.addEventListener('click', function(elements){
+        elements.preventDefault();
+        var s = document.getElementById("country").value;
+        httpsRequest = new XMLHttpRequest();
+
+        var url = 'world.php?country=';
+        httpsRequest.onreadystatechange = countryLUp
+        httpsRequest.open('GET', url+s, true);
+        httpsRequest.send();
+        console.log(s);
+    });
+
+    cityLookupbtn.addEventListener('click', function(elements){
+        elements.preventDefault();
+        var s = document.getElementById("country").value;
+        httpsRequest = new XMLHttpRequest();
+
+        var url = 'world.php?country=';
+        httpsRequest.onreadystatechange = countryLUp;
+        httpsRequest.open('GET', url+s+'&context=cities', true);
+        httpsRequest.send();
+        console.log(s);
+    });
+
+    function countryLUp(){
+        httpsRequest.onreadystatechange = function() {
+            if (httpsRequest.readyState == XMLHttpRequest.DONE) {
+                if (httpsRequest.status == 200){	
+                    var response = httpsRequest.responseText;
+                    var result = document.getElementById('result');
+                    result.innerHTML = response;
+                    console.log(response);
+                }else{
+                    alert("There was a problem with the problem with the request.");
+                }
+            }
+        }
+    }
+}; 
+
